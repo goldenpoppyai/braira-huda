5bc1e3cb1274d88bb9a006f9d49a23093f34912d
goldenpoppyai <golden.poppy.pt@gmail.com>
2025-09-18 11:04:46 +0200

    Create config.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,36 @@
+// src/lib/config.ts
+export const BOOKING_URL = "https://www.boudl.com/en/brand/braira/hotel/braira-olaya";
+
+// Optionally: create an array (ordered) of your Cloudinary images for gallery usage
+export const CLOUDINARY_GALLERY = [
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/piqoru9kgjnvvjvc50tj.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/qkmmbb7skgqf2zeebzvf.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/uthe09jcbepprowwyhlw.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/uusn8cgsobm7o6lsjdzu.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/ysofvleoyb8oou56wqhz.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/bfmyrkxszb3mlekivi2k.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/t8w12xrc7j0uqccbqc3b.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/sf8qzkcgbobiwavn48sp.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/lnjrovjh3sngg7eyjzvi.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/iilgopcluvic0oscjivl.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/vwf1cbouznffbohygoms.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/my4pvqo4ny9u72s15m5g.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/kquxcn7dtwterwyvcpfd.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/s6byga4p1nsgdztxjnhw.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/pvdisrhs0h5qejhzf6i4.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/yhvtfqeoocoulpuhsqa9.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/lyhoud0nxvtinpek5qsx.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/zrhy90hekp8mwxyqrhb4.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/hu4qea3gexrrbx2m24tp.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/yucgpjjqz0rsrtdfx7su.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/apa63vk2c6dbjayyhwkp.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/nwprveevsucizo13hvpf.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/bsqangbhbaubpex4vjya.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/n87uolqkmkwvrtqg3402.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/pt2p4dqjhd7ll1dzhu2p.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/zdfrdjny7teccuyozvwa.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/b7iyb0z1s4kj3gtgirxz.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/i5dsbxwjoxzxdcp4hior.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/rxfp1idl4lcy8nc9fmmh.jpg",
+  "https://res.cloudinary.com/dbfo2af0a/image/upload/v1720529641/boudl/uploads/imgs/blduz2frxhulxwg5hgb1.jpg"
+];
